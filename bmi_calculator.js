1cdbf8af37c2bae1cb04e60e194265d66c3c126d
Shriharsh <shriharshgautam7@gmail.com>
2023-11-18 15:17:01 +0530

    Created logic for calculating BMI

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -16,6 +16,10 @@ form.addEventListener('submit', function(e){
     else if( weight === '' || weight < 0 || isNaN(weight)){
         results.innerHTML = `Please give a valid weight : ${weight}`
     }
-
-
+    else {
+        const bmi = (weight / ((height*height)/10000)).toFixed(2)
+        //show the result
+        results.innerHTML = `Your BMI is : ${bmi}`
+    }
+    
 })
